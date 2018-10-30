ec65232d18c2c560c86025364309562842e58c8c
umesh <umesh@LAPTOP-N91UO528>
2018-10-30 09:01:23 +0530

    All code checkin

---
+++ dsfasdasd/src/test/LoginAction.java
@@ -0,0 +1,34 @@
+package test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String execute() {
+	      System.out.println("hellooooooooooooooooooo");
+	       if(username.equalsIgnoreCase("aaa")) 
+	        return "success";
+	       else
+	    	   
+	    	   return "error";
+	    }
+}
